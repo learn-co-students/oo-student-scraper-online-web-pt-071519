f49203ef0fcdcbe247a3e84a17bc69740d70ee6a
Alouiscious Media <info@reneesoul.com>
2019-09-03 11:11:15 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index = #block-views-events-block-upcoming-summary > div > div > div.view-header > div > h2
   end
 
   def self.scrape_profile_page(profile_url)

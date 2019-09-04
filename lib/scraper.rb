18548ff1edde928273e5e870b258a0b20b233255
Alouiscious Media <info@reneesoul.com>
2019-09-04 00:58:25 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,32 @@
 require 'open-uri'
 require 'pry'
-
+html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/)
 class Scraper
+# take the string of HTML returned
+# by open-uri's `open` method and convert it into a NodeSet 
+
+doc = Nokogiri::HTML(html)
+puts doc
+p doc.css("div.main-wrapper.roster").text  
 
   def self.scrape_index_page(index_url)
     index = #block-views-events-block-upcoming-summary > div > div > div.view-header > div > h2
+
+    https://learn-co-curriculum.github.io/student-scraper-test-page/#
+  
+    body > div.main-wrapper.roster > div
+  
+  <div class="student-card" id="ryan-johnson-card">
+              <a href="students/ryan-johnson.html">
+                <div class="view-profile-div">
+                  <h3 class="view-profile-text">View Profile</h3>
+                </div>
+                <div class="card-text-container">
+                  <h4 class="student-name">Ryan Johnson</h4>
+                  <p class="student-location">New York, NY</p>
+                </div>
+              </a>
+            </div>  end
   end
 
   def self.scrape_profile_page(profile_url)

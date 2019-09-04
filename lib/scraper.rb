033aabc80ce95d0002a65cc2fe33421a51095a3b
Alouiscious Media <info@reneesoul.com>
2019-09-04 14:41:45 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,18 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+ 
+
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index = #block-views-events-block-upcoming-summary > div > div > div.view-header > div > h2
+    doc = Nokogiri::HTML(open("index_url"))
+    all_students = doc.css(".roster-cards-container").text
+binding.pry
+    p all_students["name"]  
+  
   end
 
   def self.scrape_profile_page(profile_url)
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
+doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+
+# html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
+# doc.css(".roster-cards-container").text
+doc.css(".roster-cards-container").text
+binding.pry
+print doc.css(".roster-cards-container").text
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -9,7 +17,17 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    # <div class="student-card" id="ryan-johnson-card">
+  #             <a href="students/ryan-johnson.html">
+  #               <div class="view-profile-div">
+  #                 <h3 class="view-profile-text">View Profile</h3>
+  #               </div>
+  #               <div class="card-text-container">
+  #                 <h4 class="student-name">Ryan Johnson</h4>
+  #                 <p class="student-location">New York, NY</p>
+  #               </div>
+  #             </a>
+  #           </div>      
   end
 
   def add_student_attributes(attributes_hash)

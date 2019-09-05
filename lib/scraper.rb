a3655b9c8aa57c10451cc1fcae2e1af5e1a94e84
Alouiscious Media <info@reneesoul.com>
2019-09-05 10:18:03 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,29 @@
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
+# html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/#"))
+
+
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    binding.pry
-    doc = Nokogiri::HTML(open("index_url"))
-    all_students = doc.css(".roster-cards-container")
-    
-    all_students.collect do |student|
-      p student.text.strip
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+
+    doc.css(".roster-cards-container").each do |all_students| # .strip.gsub(/[\n]/,"")
+      all_students.css(".student-card a").each do |student|
+        student_url = ("#{student.attr('href')}").text
+        student_name = student.css("student-name").text
+        student_location = student.css("student-location").text
+        # binding.pry
+        students << {name: student_name, location: student_location, student_url: student_url}
+      end
     end
+    students
+  
+ 
+
+    
   end
 
   def self.scrape_profile_page(profile_url)
--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,3 @@
-doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-
-# html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
-# doc.css(".roster-cards-container").text
-doc.css(".roster-cards-container").text
-print doc.css(".roster-cards-container").text
 
 class Student
 
@@ -16,17 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    # <div class="student-card" id="ryan-johnson-card">
-  #             <a href="students/ryan-johnson.html">
-  #               <div class="view-profile-div">
-  #                 <h3 class="view-profile-text">View Profile</h3>
-  #               </div>
-  #               <div class="card-text-container">
-  #                 <h4 class="student-name">Ryan Johnson</h4>
-  #                 <p class="student-location">New York, NY</p>
-  #               </div>
-  #             </a>
-  #           </div>      
+     
   end
 
   def add_student_attributes(attributes_hash)

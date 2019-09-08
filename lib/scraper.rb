e2594f363a109e14df5cb7724ce4da7240b0b7d2
Alouiscious Media <info@reneesoul.com>
2019-09-08 13:46:35 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     doc = Nokogiri::HTML(open(index_url))
 
     doc.css('.student-card a[href]').each do |students|
-      scraped_students << {flatiron_url: students['href'], location: students.css('p').text, name: students.css('h4').text}
+      scraped_students << {profile_url: students['href'], location: students.css('p').text, name: students.css('h4').text}
     end
     scraped_students
 end
@@ -18,36 +18,38 @@ end
     scraped_student = []
     doc = Nokogiri::HTML(open(profile_url))
     doc.css(".social-icon-container").children.css("a").each do |social| 
-      scraped_student = {student_sites: => social.attribute('href').value}
+      scraped_student << {student_sites: social.attribute('href').value}
     
       student_url = {}
       scraped_student.each.with_index do |site, index|
         # binding.pry
-        if scraped_student.include?("linkedin")
-          student_url["#{scraped_student}:"] = site
-        elsif scraped_student.include?("github")
-          student_url["#{scraped_student}:"] = site
-        elsif scraped_student.include?("twitter")
-          student_url["#{scraped_student}:"] = site
-        elsif scraped_student.include?("blog")
-          student_url["#{scraped_student}:"] = site
-        # else
-        #   student_url["#{scraped_student}:"] = scraped_student[index]
-        end 
-
         # if scraped_student.include?("linkedin")
-        #   student_url["#{scraped_student[index].scan(/\w+\b/)[2]}:"] = site
+        #   student_url["#{scraped_student}:"] = site
         # elsif scraped_student.include?("github")
-        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        #   student_url["#{scraped_student}:"] = site
         # elsif scraped_student.include?("twitter")
-        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        #   student_url["#{scraped_student}:"] = site
         # elsif scraped_student.include?("blog")
-        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
-        # else
-        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = scraped_student[index].scan(/\w+\b/)
-        # end  
-      end  
-      student_url
+        #   student_url["#{scraped_student}:"] = site
+        #  else
+        #   student_url["#{scraped_student}:"] = scraped_student[index]
+        # end 
+
+        if scraped_student.include?("linkedin")
+          student_url["#{scraped_student[index].scan(/\w+\b/)[2]}:"] = site
+        elsif scraped_student.include?("github")
+          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        elsif scraped_student.include?("twitter")
+          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        elsif scraped_student.include?("blog")
+          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        else
+          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = scraped_student[index].scan(/\w+\b/)
+        end  
+        scraped_student.to_hash 
+        student_url
+      end 
+
  
     end
   end

bfdba945d336d42789326d2b3989b7de34e3a291
Alouiscious Media <info@reneesoul.com>
2019-09-08 13:11:45 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,28 +4,52 @@ require 'nokogiri'
 # html = open("https://learn-co-curriculum.github.io/student--test-page/#"))
 
 class Scraper
-  def self.scrape_index_page(index_url)
-    students = []
+  def self.scrape_index_page(index_url)   # scrapes the student index page and returns an array of hashes
+    scraped_students = []
     doc = Nokogiri::HTML(open(index_url))
 
-    # {name: doc_noko.css('h4').text#.strip.split(",  ") , location: doc_noko.css('p'),url: doc_noko.css('a div')
-    # } 
-    binding.pry
-
-    doc.css('h4').each do |student_name| # .strip.gsub(/[\n]/,"")
-      students = {name: student_name.text}
+    doc.css('.student-card a[href]').each do |students|
+      scraped_students << {profile_url: students['href'], location: students.css('p').text, name: students.css('h4').text}
     end
-    doc.css('p').each do |student_location|
-      students = {location: student_location.text}
-    end
-    doc.css('a div').each |student_url|
-      students = {student_url: student_url.text}
-    end
-    students
+    scraped_students
 end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_url)   # scrapes a student's profile page and returns a hash of attributes
+    scraped_student = []
+    doc = Nokogiri::HTML(open(profile_url))
+    doc.css(".social-icon-container").children.css("a").each do |social| 
+      scraped_student << {student_sites: social.attribute('href').value}
     
+      student_url = {}
+      scraped_student.each.with_index do |site, index|
+        # binding.pry
+        if scraped_student.include?("linkedin")
+          student_url["#{scraped_student}:"] = site
+        elsif scraped_student.include?("github")
+          student_url["#{scraped_student}:"] = site
+        elsif scraped_student.include?("twitter")
+          student_url["#{scraped_student}:"] = site
+        elsif scraped_student.include?("blog")
+          student_url["#{scraped_student}:"] = site
+        # else
+        #   student_url["#{scraped_student}:"] = scraped_student[index]
+        end 
+
+        # if scraped_student.include?("linkedin")
+        #   student_url["#{scraped_student[index].scan(/\w+\b/)[2]}:"] = site
+        # elsif scraped_student.include?("github")
+        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        # elsif scraped_student.include?("twitter")
+        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        # elsif scraped_student.include?("blog")
+        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
+        # else
+        #   student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = scraped_student[index].scan(/\w+\b/)
+        # end  
+      end  
+      student_url
+ 
+    end
   end
 
 end

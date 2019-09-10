f41febd615bbde7fe7e21e59e1ef486b364f92d6
Alouiscious Media <info@reneesoul.com>
2019-09-10 13:37:28 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,57 +1,58 @@
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
+
 # html = open("https://learn-co-curriculum.github.io/student--test-page/#"))
 
 class Scraper
-  def self.scrape_index_page(index_url)   # scrapes the student index page and returns an array of hashes
-    scraped_students = []
-    doc = Nokogiri::HTML(open(index_url))
 
+  attr_accessor :twitter, :blog, :facebook, :github, :linkedin
+  # attr_reader :profile_url, :location, :name
+
+
+  def self.scrape_index_page(index_url)   # scrapes the student index page and returns an array of hashes
+    doc = Nokogiri::HTML(open(index_url))
+    
+    students_array = []
     doc.css('.student-card a[href]').each do |students|
-      scraped_students << {profile_url: students['href'], location: students.css('p').text, name: students.css('h4').text}
+      students_array << {profile_url: students['href'], location: students.css('p').text, name: students.css('h4').text}
     end
-    scraped_students
+    students_array
 end
 
   def self.scrape_profile_page(profile_url)   # scrapes a student's profile page and returns a hash of attributes
-    scraped_student = []
-    doc = Nokogiri::HTML(open(profile_url))
-    doc.css(".social-icon-container").children.css("a").each do |social| 
-      scraped_student << {student_sites: social.attribute('href').value}
-    
-      student_url = {}
-      scraped_student.each.with_index do |site, index|
-        # binding.pry
-        # if scraped_student.include?("linkedin")
-        #   student_url["#{scraped_student}:"] = site
-        # elsif scraped_student.include?("github")
-        #   student_url["#{scraped_student}:"] = site
-        # elsif scraped_student.include?("twitter")
-        #   student_url["#{scraped_student}:"] = site
-        # elsif scraped_student.include?("blog")
-        #   student_url["#{scraped_student}:"] = site
-        #  else
-        #   student_url["#{scraped_student}:"] = scraped_student[index]
-        # end 
+    profile_doc = Nokogiri::HTML(open(profile_url))
+    url_hash = profile_doc.css("div.social-icon-container").children.css("a").map {|url| url.attribute('href').value}
+   
+    student_hash = {}
 
-        if scraped_student.include?("linkedin")
-          student_url["#{scraped_student[index].scan(/\w+\b/)[2]}:"] = site
-        elsif scraped_student.include?("github")
-          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
-        elsif scraped_student.include?("twitter")
-          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
-        elsif scraped_student.include?("blog")
-          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = site
-        else
-          student_url["#{scraped_student[index].scan(/\w+\b/)[1]}:"] = scraped_student[index].scan(/\w+\b/)
-        end  
-        scraped_student.to_hash 
-        student_url
-      end 
+    url_hash.each do |site|
+      # binding.pry 
+      if url_hash.include?("twitter")
+        student_hash[:twitter] = site
+      elsif url_hash.include?("linkedin")
+        student_hash[:linkedin] = site
+      elsif url_hash.include?("github")
+        student_hash[:github] = site
+      elsif url_hash.include?("blog")
+        student_hash[:blog] = site
 
- 
+      end
     end
+  
+   
+
+    if profile_doc.css("div.profile-quote")  
+      student_hash[:profile_quote] = profile_doc.css("div.profile-quote").text 
+    end
+    if profile_doc.css("div.bio-content.content-holder div.description-holder p")
+      student_hash[:bio] = profile_doc.css("div.bio-content.content-holder div.description-holder p").text 
+    end
+   
+
+
+      return student_hash
+    
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,24 +1,62 @@
 
-class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  class Student
 
-  @@all = []
+    attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  
+    @@all = []
+  
+    def initialize(student_hash)
+       student_hash.each {|key, value|
+        self.send(("#{key}="), value)}
+        @@all << self
+    end
+  
+    def self.create_from_collection(students_array)
+      students_array.each {|student_hash| Student.new(student_hash)}
+    end
+  
+    def add_student_attributes(attributes_hash)
+      attributes_hash.each {|key,value| self.send(("#{key}="),value)}
+    end
+  
+    def self.all
+      @@all
+    end
+  end
 
-  def initialize(student_hash)
+
+
+
+
+#   def initialize(student_hash)
+  
+#     student_hash.each do |student_det_title, student_det| 
+#       self.send("#{student_det_title}=", student_det)
+#     end
+#       @@all << self
+#       return student_hash[:name]
+
     
-  end
+#   end
 
-  def self.create_from_collection(students_array)
-     
-  end
+#   def self.create_from_collection(students_array)
+#     Scraper.scrape_index_page(students_array).each do |new_student| 
+#       # @@all << new_student.new
+#       Student.new = new_student
+#       binding.pry
+#     end
+#     # Scraper.scraped_students[[:location]
+#   end
 
-  def add_student_attributes(attributes_hash)
-    
-  end
+#   def add_student_attributes(attributes_hash)
 
-  def self.all
-    @@all
-  end
-end
+#     attributes_hash.each do |attr, value|
+#       self.send("#{attr}=", value)
+#     end
+#   end
+
+#   def self.all
+#     @@all
+#   end
 

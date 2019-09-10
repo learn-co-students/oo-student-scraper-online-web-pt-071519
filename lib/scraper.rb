e137866487e39fed3bb90853b4e7f122b74816a6
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-10 00:52:16 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+
   end
 
   def self.scrape_profile_page(profile_url)
